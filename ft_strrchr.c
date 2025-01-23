3bc25520527c086c0bdd0ee7dcd1224cbd0d4eb8
YolandaILara <yolandailara8@gmail.com>
2025-01-23 19:08:13 +0100

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,1 +1,35 @@
+#include "libft.h"
+#include <stdio.h>
+char	*ft_strrchr (const char *s, int c)
+{
+    int i = 0;
 
+	while (s[i] != '\0')
+	{
+		int i = 0;
+
+		while (s[i]!= '\0')
+		{
+			i++;
+		}
+		while (i >= 0)
+		{
+			if (s[i] == (char)c)
+                return ((char *)&s[i]);
+            i--;
+		}
+	}
+	return NULL;
+}
+
+/*int main(void)
+{
+	const char frase[] = "Viva la vida";
+	int caracter = 'V';
+	char *resultado;
+
+	printf("Antes de ft_strrchr: %s\n", frase);
+	resultado = ft_strrchr(frase, caracter);
+	printf("Después de ft_strrchr: %s\n", resultado);
+	return 0;
+}*/
